8334097bd01dfeabf446faafd6b013434f4c3823
andreykolodiy <mr.andreykolodiy@gmail.com>
2023-05-26 01:37:03 +0300

    add fix

--- Domain/ShortUrl.cs
+++ Domain/ShortUrl.cs
@@ -1,1 +1,1 @@
-namespace Domain;public class ShortUrl{    public string Destination { get; set; }    public string Path { get; set; }}
+namespace Domain;public class ShortUrl{    public string Destination { get; set; }    public string Path { get; set; }}  
